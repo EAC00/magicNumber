9286fc4d42a298ee9683ea5b2c05d8a64618f3a9
EAC00 <143907088+EAC00@users.noreply.github.com>
2023-09-14 15:24:29 -0400

    Add files via upload

---
+++ magicNumber.js
@@ -0,0 +1,44 @@
+// play gain button
+function again() {
+  window.location.reload(false);
+}
+
+
+// First, we get user input
+var entry = prompt("Enter a whole number");
+var entryNumber = parseInt(entry);
+
+alert("The number you have entered is " + entryNumber);
+
+if (isNaN(entry)) {
+  alert("Not a whole number!");
+  window.location.reload(false)
+}
+
+// The magic number begins
+var temp = entryNumber;
+var original = entryNumber;
+document.write("<ul>");
+
+
+document.write("<h1>Magic Number App</h1>")
+
+entryNumber += 9;
+document.write("<li>I added 9 to " + temp + ". The new number is " + entryNumber + "<br><br>");
+temp = entryNumber;
+
+entryNumber *= 2;
+document.write("<li>I multiplied " + temp + " by 2." + " The new number is " + entryNumber + "<br><br>");
+temp = entryNumber;
+
+entryNumber -= 4;
+document.write("<li> I subtracted " + temp + " by 4." + " The new number is " + entryNumber + "<br><br>");
+temp = entryNumber;
+
+entryNumber /= 2;
+document.write("<li> I divided " + temp + " by 2." + " The new number is " + entryNumber + "<br><br>");
+temp = entryNumber;
+
+entryNumber -= original;
+document.write("<li> I subtracted " + temp + " by your original number." + " The new number is " + entryNumber + "<br><br>");
+temp = entryNumber;
